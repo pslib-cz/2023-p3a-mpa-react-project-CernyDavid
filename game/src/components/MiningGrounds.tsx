7d923c152367eb56bd707a4a0a255eeaf06a4870
David Černý <david.cerny.021@pslib.cz>
2024-04-17 11:32:53 +0200

    added miners and drones adding and removing

--- game/src/components/MiningGrounds.tsx
+++ game/src/components/MiningGrounds.tsx
@@ -16,8 +16,13 @@ const MiningGrounds = () => {
         const crystalMiningUnits = parseInt(localStorage.getItem('crystalMiningUnits') || '0');
         const gemstoneMiningUnits = parseInt(localStorage.getItem('gemstoneMiningUnits') || '0');
         const availableMiningUnits = parseInt(localStorage.getItem('availableMiningUnits') || '0');
+        const availableSentryDrones = parseInt(localStorage.getItem('availableSentryDrones') || '0');
+        const metalSentryDrones = parseInt(localStorage.getItem('metalSentryDrones') || '0');
+        const crystalSentryDrones = parseInt(localStorage.getItem('crystalSentryDrones') || '0');
+        const gemstoneSentryDrones = parseInt(localStorage.getItem('gemstoneSentryDrones') || '0');
         dispatch({type: ActionType.SET_RESOURCES, payload: {metal, crystal, gemstone}});
         setMiningUnitsToDeploy(availableMiningUnits - metalMiningUnits - crystalMiningUnits - gemstoneMiningUnits);
+        setSentryDronesToDeploy(availableSentryDrones - metalSentryDrones - crystalSentryDrones - gemstoneSentryDrones);
 
         if (state.metalMiningUnits === 0) {
             dispatch({type: ActionType.SET_METAL_MINING_UNITS, value: metalMiningUnits});
@@ -30,6 +35,18 @@ const MiningGrounds = () => {
         if (state.gemstoneMiningUnits === 0) {
             dispatch({type: ActionType.SET_GEMSTONE_MINING_UNITS, value: gemstoneMiningUnits});
             localStorage.setItem('gemstoneMiningUnits', (gemstoneMiningUnits).toString());
+        }
+        if (state.metalSentryDrones === 0) {
+            dispatch({type: ActionType.SET_METAL_SENTRY_DRONES, value: metalSentryDrones});
+            localStorage.setItem('metalSentryDrones', (metalSentryDrones).toString());
+        }
+        if (state.crystalSentryDrones === 0) {
+            dispatch({type: ActionType.SET_CRYSTAL_SENTRY_DRONES, value: crystalSentryDrones});
+            localStorage.setItem('crystalSentryDrones', (crystalSentryDrones).toString());
+        }
+        if (state.gemstoneSentryDrones === 0) {
+            dispatch({type: ActionType.SET_GEMSTONE_SENTRY_DRONES, value: gemstoneSentryDrones});
+            localStorage.setItem('gemstoneSentryDrones', (gemstoneSentryDrones).toString());
         }
 
     }, []);
@@ -137,6 +154,78 @@ const MiningGrounds = () => {
         dispatch({type: ActionType.SET_GEMSTONE_MINING_UNITS, value: state.gemstoneMiningUnits + 1});
         localStorage.setItem('gemstoneMiningUnits', (state.gemstoneMiningUnits + 1).toString());
     }
+    const addMetalSentryDrone = () => {
+        if (sentryDronesToDeploy < 1) {
+            return;
+        }
+        setSentryDronesToDeploy(sentryDronesToDeploy - 1);
+        dispatch({type: ActionType.SET_METAL_SENTRY_DRONES, value: state.metalSentryDrones + 1});
+        localStorage.setItem('metalSentryDrones', (state.metalSentryDrones + 1).toString());
+    }
+    const addCrystalSentryDrone = () => {
+        if (sentryDronesToDeploy < 1) {
+            return;
+        }
+        setSentryDronesToDeploy(sentryDronesToDeploy - 1);
+        dispatch({type: ActionType.SET_CRYSTAL_SENTRY_DRONES, value: state.crystalSentryDrones + 1});
+        localStorage.setItem('crystalSentryDrones', (state.crystalSentryDrones + 1).toString());
+    }
+    const addGemstoneSentryDrone = () => {
+        if (sentryDronesToDeploy < 1) {
+            return;
+        }
+        setSentryDronesToDeploy(sentryDronesToDeploy - 1);
+        dispatch({type: ActionType.SET_GEMSTONE_SENTRY_DRONES, value: state.gemstoneSentryDrones + 1});
+        localStorage.setItem('gemstoneSentryDrones', (state.gemstoneSentryDrones + 1).toString());
+    }
+    const removeMetalMiningUnit = () => {
+        if (state.metalMiningUnits < 1) {
+            return;
+        }
+        setMiningUnitsToDeploy(miningUnitsToDeploy + 1);
+        dispatch({type: ActionType.SET_METAL_MINING_UNITS, value: state.metalMiningUnits - 1});
+        localStorage.setItem('metalMiningUnits', (state.metalMiningUnits - 1).toString());
+    }
+    const removeCrystalMiningUnit = () => {
+        if (state.crystalMiningUnits < 1) {
+            return;
+        }
+        setMiningUnitsToDeploy(miningUnitsToDeploy + 1);
+        dispatch({type: ActionType.SET_CRYSTAL_MINING_UNITS, value: state.crystalMiningUnits - 1});
+        localStorage.setItem('crystalMiningUnits', (state.crystalMiningUnits - 1).toString());
+    }
+    const removeGemstoneMiningUnit = () => {
+        if (state.gemstoneMiningUnits < 1) {
+            return;
+        }
+        setMiningUnitsToDeploy(miningUnitsToDeploy + 1);
+        dispatch({type: ActionType.SET_GEMSTONE_MINING_UNITS, value: state.gemstoneMiningUnits - 1});
+        localStorage.setItem('gemstoneMiningUnits', (state.gemstoneMiningUnits - 1).toString());
+    }
+    const removeMetalSentryDrone = () => {
+        if (state.metalSentryDrones < 1) {
+            return;
+        }
+        setSentryDronesToDeploy(sentryDronesToDeploy + 1);
+        dispatch({type: ActionType.SET_METAL_SENTRY_DRONES, value: state.metalSentryDrones - 1});
+        localStorage.setItem('metalSentryDrones', (state.metalSentryDrones - 1).toString());
+    }
+    const removeCrystalSentryDrone = () => {
+        if (state.crystalSentryDrones < 1) {
+            return;
+        }
+        setSentryDronesToDeploy(sentryDronesToDeploy + 1);
+        dispatch({type: ActionType.SET_CRYSTAL_SENTRY_DRONES, value: state.crystalSentryDrones - 1});
+        localStorage.setItem('crystalSentryDrones', (state.crystalSentryDrones - 1).toString());
+    }
+    const removeGemstoneSentryDrone = () => {
+        if (state.gemstoneSentryDrones < 1) {
+            return;
+        }
+        setSentryDronesToDeploy(sentryDronesToDeploy + 1);
+        dispatch({type: ActionType.SET_GEMSTONE_SENTRY_DRONES, value: state.gemstoneSentryDrones - 1});
+        localStorage.setItem('gemstoneSentryDrones', (state.gemstoneSentryDrones - 1).toString());
+    }
 
     const getClickAmount = () => {
         return Math.pow(2, state.mainframeLevel);
@@ -148,14 +237,23 @@ const MiningGrounds = () => {
             <p>Metal: {state.resources.metal}</p>
             <button onClick={() => mineMetal(getClickAmount() * 10)}>Mine Metal</button>
             <button onClick={() => addMetalMiningUnit()}>Add Miner</button>
+            <button onClick={() => addMetalSentryDrone()}>Add Sentry Drone</button>
+            <button onClick={() => removeMetalMiningUnit()}>Remove Miner</button>
+            <button onClick={() => removeMetalSentryDrone()}>Remove Sentry Drone</button>
             <p>{state.metalMiningUnits} mining metal.</p>
             <p>Crystal: {state.resources.crystal}</p>
             <button onClick={() => mineCrystal(getClickAmount() * 5)}>Mine Crystal</button>
             <button onClick={() => addCrystalMiningUnit()}>Add Miner</button>
+            <button onClick={() => addCrystalSentryDrone()}>Add Sentry Drone</button>
+            <button onClick={() => removeCrystalMiningUnit()}>Remove Miner</button>
+            <button onClick={() => removeCrystalSentryDrone()}>Remove Sentry Drone</button>
             <p>{state.crystalMiningUnits} mining crystal.</p>
             <p>Gemstone: {state.resources.gemstone}</p>
             <button onClick={() => mineGemstone(getClickAmount()* 2)}>Mine Gemstone</button>
             <button onClick={() => addGemstoneMiningUnit()}>Add Miner</button>
+            <button onClick={() => addGemstoneSentryDrone()}>Add Sentry Drone</button>
+            <button onClick={() => removeGemstoneMiningUnit()}>Remove Miner</button>
+            <button onClick={() => removeGemstoneSentryDrone()}>Remove Sentry Drone</button>
             <p>{state.gemstoneMiningUnits} mining gemstone.</p>
             <h2>Automation</h2>
             <p>Mining units: {miningUnitsToDeploy}, level: {state.miningUnitsLevel}</p>
